8ae29e6582430204a828954eeba8cf0f0c66582a
TheoRev <hrevillafernandez79@gmail.com>
2017-06-19 20:08:15 -0500

    restaurando proyecto

--- target/generated-sources/annotations/com/hrevfdz/models/Suppliers_.java
+++ target/generated-sources/annotations/com/hrevfdz/models/Suppliers_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-19T01:16:56")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-19T19:22:55")
 @StaticMetamodel(Suppliers.class)
 public class Suppliers_ { 
 
